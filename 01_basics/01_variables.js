8e8e707184a94af28a4c4843421c6c10c935da44
Swanand Tamaskar <tamaskarswanand@gmail.com>
2024-01-11 23:29:39 +0000

    learning

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+
+let accountEmail = "hfskfh@gfhf.cmn"
+
+var accountPassword = "12344"
+
+accountCity = "jaipur"
+
+// accountId = 2; // not allowed
+
+accountEmail = "hskfhk@hfsl.fhf"
+
+accountPassword = "647485"
+
+accountCity = "pune"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword ,accountCity])
